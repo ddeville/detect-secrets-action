c9355b19ccab4679863738ad2d994096f8b0e79c
nevermoe <nevermoe@users.noreply.github.com>
2020-04-15 15:55:24 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 output=$(detect-secrets scan)
 echo "::set-output name=output::$output"
-lines=echo $output | jq .results | wc -l
+lines=`echo $output | jq .results | wc -l`
 if [ "$lines" -gt 1 ]; then
     echo "Secret Check Failed"
     exit 1
