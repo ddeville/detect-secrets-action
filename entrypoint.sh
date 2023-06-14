5d8c7c68e7b89aefce02e21c65855d523e5d08b3
Damien Deville <damien@ddeville.me>
2023-06-13 21:53:26 -0700

    Don't check lock files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 set -e
-output=$(detect-secrets scan)
+output=$(detect-secrets scan --exclude-files '.*-lock\.json$')
 echo "::set-output name=output::$output"
-lines=`echo $output | jq .results | wc -l`
+lines=$(echo "$output" | jq .results | wc -l)
 if [ "$lines" -gt 1 ]; then
-    echo "Secret Check Failed"
-    exit 1
+  echo "Secret Check Failed"
+  exit 1
 fi
