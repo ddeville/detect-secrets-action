f5ba8ee1e5e1e7f5465dd1f72b7121807e312433
Damien Deville <damien@ddeville.me>
2026-03-16 20:57:21 -0700

    Fix deprecated use of set-output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 set -e
 output=$(detect-secrets scan --exclude-files '.*-lock\.json$')
-echo "::set-output name=output::$output"
+{
+  printf 'output<<__OUTPUT__\n'
+  printf '%s\n' "$output"
+  printf '__OUTPUT__\n'
+} >> "$GITHUB_OUTPUT"
 lines=$(echo "$output" | jq .results | wc -l)
 if [ "$lines" -gt 1 ]; then
   echo "Secret Check Failed"
